c361eb8d36a93ec33521e38ee6f5aa61c0d91c97
ricbolz <hendricovbolang@gmail.com>
2020-12-30 21:24:18 +0900

    finding model instance part 1 tosave

--- server/test.js
+++ server/test.js
@@ -1,14 +1,23 @@
 var models = require('./server.js').models;
 
-models.Profile.findOrCreate({name: 'Rico1'}, (err, profile) => {
-    if(err){
-        console.log("There was an error".err);
-    }
-    else if(profile){
-        profile.email = 'rico@rico2.com';
-        profile.save((ue,updated) => {
-            console.log("Updated?", updated)
-        })
-    }
-    
+/*
+var toSave = [
+    {name: 'Rico', email: 'rico@rico.com'},
+    {name: 'Rico1', email: 'ricoxx@rico.com'},
+    {name: 'Rico2', email: 'ricoss@rico.com'},
+    {name: 'Ri', email: 'ri@rico.com'},
+    {name: 'Rico3', email: 'ricoddd@rico.com'},
+    {name: 'Rico4', email: 'ricowww@rico.com'},
+    {name: 'Rico5', email: 'ricokkkk@rico.com'},
+    {name: 'Rico6', email: 'ricollll@rico.com'},
+    {name: 'dare', email: 'omae@rico.com'},
+    {name: 'Ryooo', email: 'juuu@rico.com'},
+];
+
+toSave.map(obj => {
+    models.Profile.create(obj, (err,created) => {
+        console.log("Created?", created);
+    })
 })
+
+*/
