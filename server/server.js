36be81e2b66a5aa6514855df4f4abb4c1b51c8bd
ricbolz <hendricovbolang@gmail.com>
2021-04-02 23:23:36 +0900

    add roles and applying access control

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 
 'use strict';
 
+const { Role } = require('loopback');
 const loopback = require('loopback');
 const boot = require('loopback-boot');
 
@@ -69,4 +70,31 @@ app.models.user.afterRemote('create', (ctx,user, next) => {
     next();
   })
   
+});
+
+app.models.Role.find({where: {name: 'admin'}}, (err, role) => {
+  if(!err && role) {
+    console.log('No error, role is', role);
+    if(role.length === 0) {
+       app.models.Role.create({
+         name: 'admin',
+       }, (err2, result) => {
+         if(!err2 && result) {
+
+          app.models.user.findOne((usererr, user) => {
+            if (!usererr && user) {
+              result.principals.create({
+                principalType: app.models.RoleMapping.USER,
+                principalId: user.id
+              }, (err3, principal) => {
+                console.log("Created principal", err3, principal);
+              });
+
+            }
+          })
+           
+         }
+       })
+    }
+  }
 });
